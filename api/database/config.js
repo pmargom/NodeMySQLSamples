99588378fd8c399141403fa286946c758415d9e3
Pedro Martín Gómez <ligartolabs@hotmail.com>
2017-03-02 13:11:18 +0000

    Added first mysql query for testing

---
+++ api/database/config.js
@@ -0,0 +1,9 @@
+
+var config = module.exports = {
+    database: {
+        host    : 'eu-cdbr-azure-west-d.cloudapp.net',
+        user    : 'bf8fe44cc88880',
+        password: '9e610bd0',
+        database: 'acsm_764524dac413151'
+    }
+};
--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var dataApi = require('./api/data');
+
 var app = express();
 
 // view engine setup
@@ -24,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// api
+app.use('/api/v1/data', dataApi());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
