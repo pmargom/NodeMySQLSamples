79d97cc441c3a510ab61073445d8d482ad265bd5
Pedro Martín Gómez <ligartolabs@hotmail.com>
2017-03-02 13:07:59 +0000

    Clean up

--- api/database/config.js
+++
@@ -1,9 +0,0 @@
-
-var config = module.exports = {
-    database: {
-        host    : 'eu-cdbr-azure-west-d.cloudapp.net',
-        user    : 'bf8fe44cc88880',
-        password: '9e610bd0',
-        database: 'acsm_764524dac413151'
-    }
-};
